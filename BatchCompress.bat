dbb36124c6ab4a746ec2d5aa8c6d13ddb03994c8
Jason-M-Kane <108639908+Jason-M-Kane@users.noreply.github.com>
2022-07-03 23:20:44 -0400

    Add files via upload

---
+++ BatchCompress.bat
@@ -0,0 +1,227 @@
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 0 1 1
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 0 1 2
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 0 1 4
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 0 1 8
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 0 1 16
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 0 1 32
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 0 1 48
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 0 1 64
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 0 1 128
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 0 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 0 1 512
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 0 1 1024
+
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 1 1 1
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 1 1 2
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 1 1 4
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 1 1 8
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 1 1 16
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 1 1 32
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 1 1 48
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 1 1 64
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 1 1 128
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 1 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 1 1 512
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 1 1 1024
+
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 2 1 1
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 2 1 2
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 2 1 4
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 2 1 8
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 2 1 16
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 2 1 32
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 2 1 48
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 2 1 64
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 2 1 128
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 2 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 2 1 512
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 2 1 1024
+
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 3 1 1
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 3 1 2
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 3 1 4
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 3 1 8
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 3 1 16
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 3 1 32
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 3 1 48
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 3 1 64
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 3 1 128
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 3 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 3 1 512
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 3 1 1024
+
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 4 1 1    8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 4 1 2    8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 4 1 4    8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 4 1 8    8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 4 1 16   8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 4 1 32   8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 4 1 48   8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 4 1 64   8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 4 1 128  8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 4 1 256  8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 4 1 512  8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 4 1 1024 8 20
+
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 5 1 1    8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 5 1 2    8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 5 1 4    8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 5 1 8    8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 5 1 16   8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 5 1 32   8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 5 1 48   8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 5 1 64   8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 5 1 128  8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 5 1 256  8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 5 1 512  8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 5 1 1024 8 20
+
+
+
+
+
+
+
+
+
+compressionTest.exe C:\Data_Sets\DB_Pedia_Thumbnail_Images\images_en.nq 0 1 256
+compressionTest.exe C:\Data_Sets\DB_Pedia_Thumbnail_Images\images_en.nq 1 1 256
+compressionTest.exe C:\Data_Sets\DB_Pedia_Thumbnail_Images\images_en.nq 2 1 256
+compressionTest.exe C:\Data_Sets\DB_Pedia_Thumbnail_Images\images_en.nq 3 1 256
+compressionTest.exe C:\Data_Sets\DB_Pedia_Thumbnail_Images\images_en.nq 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\DB_Pedia_Thumbnail_Images\images_en.nq 5 1 256 8 20
+
+compressionTest.exe C:\Data_Sets\E-Coli-c227-11_Raw_HDF5\e-coli-c227-11-raw-reads-1.2.2beta.tar 0 1 256
+compressionTest.exe C:\Data_Sets\E-Coli-c227-11_Raw_HDF5\e-coli-c227-11-raw-reads-1.2.2beta.tar 1 1 256
+compressionTest.exe C:\Data_Sets\E-Coli-c227-11_Raw_HDF5\e-coli-c227-11-raw-reads-1.2.2beta.tar 2 1 256
+compressionTest.exe C:\Data_Sets\E-Coli-c227-11_Raw_HDF5\e-coli-c227-11-raw-reads-1.2.2beta.tar 3 1 256
+compressionTest.exe C:\Data_Sets\E-Coli-c227-11_Raw_HDF5\e-coli-c227-11-raw-reads-1.2.2beta.tar 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\E-Coli-c227-11_Raw_HDF5\e-coli-c227-11-raw-reads-1.2.2beta.tar 5 1 256 8 20
+
+
+
+
+
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 0 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 1 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 2 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 3 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-0.csv 5 1 256 8 20
+
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-1.csv 0 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-1.csv 1 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-1.csv 2 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-1.csv 3 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-1.csv 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-1.csv 5 1 256 8 20
+
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-2.csv 0 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-2.csv 1 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-2.csv 2 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-2.csv 3 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-2.csv 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-2.csv 5 1 256 8 20
+
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-3.csv 0 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-3.csv 1 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-3.csv 2 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-3.csv 3 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-3.csv 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-3.csv 5 1 256 8 20
+
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-4.csv 0 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-4.csv 1 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-4.csv 2 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-4.csv 3 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-4.csv 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-4.csv 5 1 256 8 20
+
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-5.csv 0 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-5.csv 1 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-5.csv 2 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-5.csv 3 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-5.csv 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-5.csv 5 1 256 8 20
+
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-6.csv 0 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-6.csv 1 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-6.csv 2 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-6.csv 3 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-6.csv 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-6.csv 5 1 256 8 20
+
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-7.csv 0 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-7.csv 1 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-7.csv 2 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-7.csv 3 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-7.csv 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-7.csv 5 1 256 8 20
+
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-8.csv 0 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-8.csv 1 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-8.csv 2 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-8.csv 3 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-8.csv 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-8.csv 5 1 256 8 20
+
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-9.csv 0 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-9.csv 1 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-9.csv 2 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-9.csv 3 1 256
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-9.csv 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\Google-Books-1-Gram\googlebooks-eng-all-1gram-20090715-9.csv 5 1 256 8 20
+
+
+
+
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_1.fastq 0 1 256
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_1.fastq 1 1 256
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_1.fastq 2 1 256
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_1.fastq 3 1 256
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_1.fastq 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_1.fastq 5 1 256 8 20
+
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_2.fastq 0 1 256
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_2.fastq 1 1 256
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_2.fastq 2 1 256
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_2.fastq 3 1 256
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_2.fastq 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_2.fastq 5 1 256 8 20
+
+
+
+
+compressionTest.exe C:\Data_Sets\NY-Hudson-Sidescan-Sonar-from-NOAA\xtf_Files.tar 0 1 256
+compressionTest.exe C:\Data_Sets\NY-Hudson-Sidescan-Sonar-from-NOAA\xtf_Files.tar 1 1 256
+compressionTest.exe C:\Data_Sets\NY-Hudson-Sidescan-Sonar-from-NOAA\xtf_Files.tar 2 1 256
+compressionTest.exe C:\Data_Sets\NY-Hudson-Sidescan-Sonar-from-NOAA\xtf_Files.tar 3 1 256
+compressionTest.exe C:\Data_Sets\NY-Hudson-Sidescan-Sonar-from-NOAA\xtf_Files.tar 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\NY-Hudson-Sidescan-Sonar-from-NOAA\xtf_Files.tar 5 1 256 8 20
+
+
+compressionTest.exe C:\Data_Sets\WikiPedia-Pages-Articles-06-01-2012\enwiki-20120601-pages-articles.xml 0 1 256
+compressionTest.exe C:\Data_Sets\WikiPedia-Pages-Articles-06-01-2012\enwiki-20120601-pages-articles.xml 1 1 256
+compressionTest.exe C:\Data_Sets\WikiPedia-Pages-Articles-06-01-2012\enwiki-20120601-pages-articles.xml 2 1 256
+compressionTest.exe C:\Data_Sets\WikiPedia-Pages-Articles-06-01-2012\enwiki-20120601-pages-articles.xml 3 1 256
+compressionTest.exe C:\Data_Sets\WikiPedia-Pages-Articles-06-01-2012\enwiki-20120601-pages-articles.xml 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\WikiPedia-Pages-Articles-06-01-2012\enwiki-20120601-pages-articles.xml 5 1 256 8 20
+
+
+compressionTest.exe C:\Data_Sets\WikiPedia-Pages-Articles-06-01-2012\enwiki-20120601-pages-articles.bz2 0 1 256
+compressionTest.exe C:\Data_Sets\WikiPedia-Pages-Articles-06-01-2012\enwiki-20120601-pages-articles.bz2 1 1 256
+compressionTest.exe C:\Data_Sets\WikiPedia-Pages-Articles-06-01-2012\enwiki-20120601-pages-articles.bz2 2 1 256
+compressionTest.exe C:\Data_Sets\WikiPedia-Pages-Articles-06-01-2012\enwiki-20120601-pages-articles.bz2 3 1 256
+compressionTest.exe C:\Data_Sets\WikiPedia-Pages-Articles-06-01-2012\enwiki-20120601-pages-articles.bz2 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\WikiPedia-Pages-Articles-06-01-2012\enwiki-20120601-pages-articles.bz2 5 1 256 8 20
+
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_1.fastq.gz 0 1 256
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_1.fastq.gz 1 1 256
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_1.fastq.gz 2 1 256
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_1.fastq.gz 3 1 256
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_1.fastq.gz 4 1 256 8 20
+compressionTest.exe C:\Data_Sets\Mouse-Genome-Strain\ERR007772_1.fastq.gz 5 1 256 8 20
+
+
+
+pause
